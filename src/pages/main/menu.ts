bc410f05c7f189aafcdd9355bba152afbb1cadf4
IlyaK <ilyxakotsur@gmail.com>
2022-08-18 23:45:01 +0300

    feat: create main page and structure of project

---
+++ src/pages/main/menu.ts
@@ -0,0 +1,18 @@
+export default class Menu {
+  menuStart() {
+    const menuBtn = document.querySelector('.menu__title');
+    const menu = document.querySelector('.menu');
+    menuBtn?.addEventListener('click', () => {
+      const menuImage = menuBtn.querySelector('img');
+      if (menu && menuImage) {
+        if (menu.classList.contains('menu--enabled')) {
+          menu.classList.remove('menu--enabled');
+          menuImage.src = './assets/images/menu-icon.svg';
+        } else {
+          menu.classList.add('menu--enabled');
+          menuImage.src = './assets/images/cross-icon.svg';
+        }
+      }
+    });
+  }
+}
